477597051719adb5d89b1da4cfc6eff1f2649bd0
Mario <marionuman@gmail.com>
2024-09-25 13:38:19 +0200

    Ejercicio 6 Completo

--- src/main/java/com/campusdual/classroom/Exercise06.java
+++ src/main/java/com/campusdual/classroom/Exercise06.java
@@ -9,6 +9,8 @@ public class Exercise06 {
         Calendar now = Calendar.getInstance();
         int month = now.get(Calendar.MONTH) + 1;
         int hour = now.get(Calendar.HOUR_OF_DAY);
+        checkHour(hour);
+        checkSeason(month);
     }
 
     //TODO ↓
@@ -20,34 +22,81 @@ public class Exercise06 {
         {
             switch (hour) {
                 case 21:
+                    System.out.println("It's night");
+                    break;
                 case 22:
+                    System.out.println("It's night");
+                    break;
                 case 23:
+                    System.out.println("It's night");
+                    break;
                 case 0:
+                    System.out.println("It's night");
+                    break;
                 case 1:
+                    System.out.println("It's night");
+                    break;
                 case 2:
+                    System.out.println("It's night");
+                    break;
                 case 3:
+                    System.out.println("It's night");
+                    break;
                 case 4:
+                    System.out.println("It's night");
+                    break;
                 case 5:
+                    System.out.println("It's night");
+                    break;
                 case 6: {
-
+                    System.out.println("It's night");
+                    break;
                 }
                 case 7:
+                    System.out.println("It's morning");
+                    break;
                 case 8:
+                    System.out.println("It's morning");
+                    break;
                 case 9:
+                    System.out.println("It's morning");
+                    break;
                 case 10:
+                    System.out.println("It's morning");
+                    break;
                 case 11:
+                    System.out.println("It's morning");
+                    break;
                 case 12:
+                    System.out.println("It's morning");
+                    break;
                 case 13: {
+                    System.out.println("It's morning");
+                    break;
 
 
                 }
                 case 14:
+                    System.out.println("It's afternoon");
+                    break;
                 case 15:
+                    System.out.println("It's afternoon");
+                    break;
                 case 16:
+                    System.out.println("It's afternoon");
+                    break;
                 case 17:
+                    System.out.println("It's afternoon");
+                    break;
                 case 18:
+                    System.out.println("It's afternoon");
+                    break;
                 case 19:
+                    System.out.println("It's afternoon");
+                    break;
                 case 20: {
+                    System.out.println("It's afternoon");
+                    break;
 
 
                 }
@@ -65,27 +114,47 @@ public class Exercise06 {
     public static void checkSeason(int month) {
         switch (month) {
             case 1:
+                System.out.println("Winter");
+                break;
             case 2:
+                System.out.println("Winter");
+                break;
             case 3: {
-
+                System.out.println("Winter");
+                break;
 
             }
             case 4:
+                System.out.println("Spring");
+                break;
             case 5:
+                System.out.println("Spring");
+                break;
             case 6: {
-
+                System.out.println("Spring");
+                break;
 
             }
             case 7:
+                System.out.println("Summer");
+                break;
             case 8:
+                System.out.println("Summer");
+                break;
             case 9: {
-
+                System.out.println("Summer");
+                break;
 
             }
             case 10:
+                System.out.println("Autumn");
+                break;
             case 11:
+                System.out.println("Autumn");
+                break;
             case 12: {
-
+                System.out.println("Autumn");
+                break;
 
             }
             default:
